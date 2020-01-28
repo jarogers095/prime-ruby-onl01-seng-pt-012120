0d2805044c8de3bda7dd901df18fc9ba05b4ecbf
James Rogers <jarogers095@gmail.com>
2020-01-28 07:42:16 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,2 +1,11 @@
 def prime?(num)
+  result = true
+  range = (2..(num / 2)).to_a
+  range.each do |divisor|
+    if num % divisor == 0
+      result = false
+    end
+  end
   
+  return result
+end
